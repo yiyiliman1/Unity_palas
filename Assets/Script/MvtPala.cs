94f81214bc3cb71dbd3da949c3f3f7085b336c42
yiyiliman1 <yiyiliman113@gmail.com>
2024-10-23 14:12:03 +0200

    Subida

---
+++ Assets/Script/MvtPala.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MvtPala : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private float x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");    // atl + flechas para mover el codigo
+        y = Input.GetAxis("Vertical"); // control + k + c reorganizar el codigo
+        // para comertar todo control + k + c (es necesario seleccionar todo previamente)
+        // para descomertar todo control + k + u (es necesario seleccionar todo previamente)
+    }
+    private void FixedUpdate()
+    {
+        // cada vez que quieras modificar el rigibody aqui
+        rb.velocity = new Vector2(x, y) * speed; // 2 de 2d
+        // ajustas la velocidad en los componentes de unity*
+    }
+}
